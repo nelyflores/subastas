55333f28f6141f994c2822db05e0a6fe84727e2c
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-10-18 16:22:40 -0500

    suma cantidad seleccionada en detalle de producto

--- js/detallesubasta.js
+++ js/detallesubasta.js
@@ -2,17 +2,28 @@ $(window).load(function() {
   var cargarPagina = function(){
     $('#detalles').modal('show');
     $('.ofertar').click(ofertar);
-    $('.aceptar').click(ofertar);
+    $('.aceptar').click(cerrarModal);
   };
 
   var ofertar = function(e){
     e.preventDefault();
-    var valor = $('#ofertar').val();
-
-    $(".cantidad-ofertada").text(valor);
-
-    if('.')
+    var valorOfertado = $('#ofertar').val();
+    var aceptar = $(".cantidad-ofertada").text(valorOfertado);
   }
+
+  var cerrarModal = function(e){
+    e.preventDefault();
+    var precioInicio = $('#precio-inicial').text();
+    var valorOfertado = $('#ofertar').val();
+
+    var suma1 = parseInt(precioInicio) + parseInt(valorOfertado);
+
+    $('#confirmacion').modal('hide');
+    $('.cantidad-ofertadaTotal').append(suma1);
+  }
+
+
+
 
   $(document).ready(cargarPagina);
 });
--- js/form-producto.js
+++ js/form-producto.js
@@ -46,11 +46,6 @@ $(window).load(function() {
   $(document).ready(cargarPagina);
 });
 
-
-// (function(){
-//
-// })();
-
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
@@ -83,6 +78,3 @@ function initMap() {
       'Error: Your browser doesn\'t support geolocation.');
     }
   }
-
-
- 
