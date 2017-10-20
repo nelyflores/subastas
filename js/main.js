add5c47edc81c192755f0cd0b9cbe11bbc596497
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-10-19 21:08:18 -0500

    menu de hamburguesa y redireccion al index correcto

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-$(document).ready(function() {              
-    $('i.glyphicon-thumbs-up, i.glyphicon-thumbs-down').click(function(){    
+$(document).ready(function() {
+    $('i.glyphicon-thumbs-up, i.glyphicon-thumbs-down').click(function(){
         var $this = $(this),
-        c = $this.data('count');    
+        c = $this.data('count');
         if (!c) c = 0;
         c++;
         $this.data('count',c);
         $('#'+this.id+'-bs3').html(c);
-    });      
+    });
     $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox();
-    });                                        
-}); 
+    });
+
+    var sideslider = $('[data-toggle=collapse-side]');
+    var sel = sideslider.attr('data-target');
+    var sel2 = sideslider.attr('data-target-2');
+    sideslider.click(function(event){
+      $(sel).toggleClass('in');
+      $(sel2).toggleClass('out');
+    });                                       
+});
