321bb36555757989ee8e3dadf09caaddec395dbb
nelyflores <nely.afloresr@gmail.com>
2017-10-19 23:57:56 -0500

    agrega estilos a los precios

--- js/mostrarsubastas.js
+++ js/mostrarsubastas.js
@@ -10,16 +10,16 @@ function inicializar(){
 var plantillaSubasta= '<article class="col-xs-6">'+
             '<div class="panel panel-default">'+
                 '<div class="panel-body">'+
-                    '<a href="http://lorempixel.com/350/350/nature/" title="Nature Portfolio" class="zoom" data-title="Amazing Nature" data-footer="The beauty of nature" data-type="image" data-toggle="lightbox">'+
+                    '<a href="http://lorempixel.com/350/350/nature/" title="--titulo--" class="zoom" data-title="Amazing Nature" data-footer="The beauty of nature" data-type="image" data-toggle="lightbox">'+
                         '<img src="--imagen--" alt="Nature Portfolio" />'+
-                        '<span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>'+
+                        '<span class="overlay"><i class=""glyphicon glyphicon-search""></i></span>'+
                     '</a>'+
                 '</div>'+
                 '<div class="panel-footer">'+
-                    '<h4>Precio Actual $100.00</h4>'+
-                      '<h4>Precio Inicial  --precio-- </h4>'+
+                    '<p>Precio Actual $100.00</p>'+
+                      '<p>Precio Inicial <span class="preciobase"> --precio-- </span></p>'+
                     '<span class="pull-right">'+
-                        '<a href=" detallesubasta.html" class="enviarDetalles" data-detallesubasta="--llave--">ir a subasta<i id="like1" class="glyphicon glyphicon-plus-sign"></i> </a><div id="like1-bs3"></div>'+
+                        '<a href=" detallesubasta.html" class="enviarDetalles" data-detallesubasta="--llave--"  id="irSubasta">ir a subasta<i id="like1" class="glyphicon glyphicon-plus"aria-hidden="true"></i></a><div id="like1-bs3"></div>'+
                    '</span>'+
                 '</div>'+
             '</div>'+
@@ -31,6 +31,7 @@ function mostrarSubastasDeFirebase(){
 		var tarjetasSubastas="";
 		for (var key in datos){
 			tarjetasSubastas += plantillaSubasta.replace("--imagen--",datos[key].url1) 
+								.replace("--titulo--",datos[key].titulo)
 								.replace("--precio--",datos[key].precio)
 								.replace("--llave--",key)
 
@@ -55,6 +56,6 @@ function mostrarDetalle(){
 		
 		console.log(datos.titulo);
 	})
-	alert("la subasta seleccionada es :" + refDeSubastaSeleccionada );
+	//alert("la subasta seleccionada es :" + refDeSubastaSeleccionada );
 	localStorage.setItem("tarjetaSeleccionada", refDeSubastaSeleccionada);
 }
